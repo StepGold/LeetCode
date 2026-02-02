d98e9f52f0ed5794691855606bc79789b9da0e08
Step <stepgold0@gmail.com>
2026-02-02 09:53:17 +0300

    Time: 4 ms (80.31%) | Memory: 19.4 MB (9.61%) - LeetSync

--- 9-palindrome-number/palindrome-number.py
+++ 9-palindrome-number/palindrome-number.py
@@ -2,7 +2,10 @@ class Solution:
     def isPalindrome(self, x: int) -> bool:
         if x < 0:
             return False
-        li = str(x)
-        y = int(li[::-1])
+        y = 0
+        i = 1
+        while i <= x:
+            y = y*10 + ((x % (i * 10)) // i)
+            i *= 10
         return x - y == 0
         
