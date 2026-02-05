4d319ea63914a23c5b1e5fba7b704de92a8c6710
Step <stepgold0@gmail.com>
2026-02-06 01:28:12 +0300

    Time: 64 ms (33.33%) | Memory: 19.4 MB (11.39%) - LeetSync

--- 3651-transformed-array/transformed-array.py
+++ 3651-transformed-array/transformed-array.py
@@ -1,3 +1,7 @@
 class Solution:
     def constructTransformedArray(self, nums: List[int]) -> List[int]:
-        return [nums[(i + nums[i]) % len(nums)] for i in range(len(nums))]
+        n = len(nums)
+        r = []
+        for i, v in enumerate(nums):
+            r.append(nums[(i + v) % n])
+        return r
