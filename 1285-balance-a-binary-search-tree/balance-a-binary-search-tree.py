03a34c1290076a01dd5a4f984093193726e21658
Step <stepgold0@gmail.com>
2026-02-10 00:36:13 +0300

    Time: 31 ms (68.47%) | Memory: 26.4 MB (21.31%) - LeetSync

---
+++ 1285-balance-a-binary-search-tree/balance-a-binary-search-tree.py
@@ -0,0 +1,32 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def balanceBST(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        li = []
+        def convert(r):
+            li.append(r.val)
+            if r.left:
+                convert(r.left)
+            if r.right:
+                convert(r.right)
+        
+        convert(root)
+        li.sort()
+
+        def create(a, b):
+            if a > b:
+                return None
+            
+            m = (a + b) // 2
+            left = create(a, m-1)
+            right = create(m+1, b)
+
+            r = TreeNode(li[m], left, right)
+
+            return r
+        
+        return create(0, len(li) - 1)
