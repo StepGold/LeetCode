c82267a5fa1fa4ee20b985ce1e5be6af5dca9f54
Step <stepgold0@gmail.com>
2026-02-15 09:57:14 +0300

    Time: 4 ms (16.36%) | Memory: 19.6 MB (12.44%) - LeetSync

--- 67-add-binary/add-binary.py
+++ 67-add-binary/add-binary.py
@@ -1,17 +1,19 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
-        a = [int(i) for i in a[::-1]]
-        b = [int(i) for i in b[::-1]]
+        a = [int(i) for i in a]
+        b = [int(i) for i in b]
         c = []
         prev = 0
-        while a or b:
+        aa = len(a)
+        bb = len(b)
+        while aa or bb:
             cur = 0
-            if a:
-                cur += a[0]
-                a.pop(0)
-            if b:
-                cur += b[0]
-                b.pop(0)
+            if aa:
+                cur += a[aa - 1]
+                aa -= 1
+            if bb:
+                cur += b[bb - 1]
+                bb -= 1
             cur += prev
             prev = cur // 2
             cur %= 2
