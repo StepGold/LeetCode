a2ee7942e6c8b8f897347874bd08eea4a6166017
Step <stepgold0@gmail.com>
2026-02-16 18:55:41 +0300

    Time: 72 ms (88.05%) | Memory: 19.6 MB (31.81%) - LeetSync

--- 815-champagne-tower/champagne-tower.py
+++ 815-champagne-tower/champagne-tower.py
@@ -1,12 +1,16 @@
 class Solution(object):
-    def champagneTower(self, poured, query_row, query_glass):
-        A = [[0] * k for k in range(1, 102)]
-        A[0][0] = poured
-        for r in range(query_row + 1):
-            for c in range(r+1):
-                q = (A[r][c] - 1.0) / 2.0
-                if q > 0:
-                    A[r+1][c] += q
-                    A[r+1][c+1] += q
-
-        return min(1, A[query_row][query_glass])
+    def champagneTower(self, p, row, col):
+        li = [[0] * n for n in range(1, row+3)]
+        li[0][0] = p
+        for i in range(row+1):
+            for j in range(i + 1):
+                a = li[i][j]
+                if a > 1:
+                    li[i][j] = 1
+                    a = (a - 1) / 2
+                    li[i+1][j] += a
+                    li[i+1][j+1] += a
+        
+        return li[row][col]
+                
+        
