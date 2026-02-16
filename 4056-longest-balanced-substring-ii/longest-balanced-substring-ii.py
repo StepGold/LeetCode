94dbf81c5504a035f80955e284a9c89aac3e8fd2
Step <stepgold0@gmail.com>
2026-02-17 00:13:39 +0300

    Time: 835 ms (70.55%) | Memory: 45.3 MB (92.70%) - LeetSync

--- 4056-longest-balanced-substring-ii/longest-balanced-substring-ii.py
+++ 4056-longest-balanced-substring-ii/longest-balanced-substring-ii.py
@@ -1,66 +1,68 @@
 class Solution:
-    def mono(self, s: str) -> int:
-        if not s:
-            return 0
-        cnt = 1
-        ans = 1
-        for i in range(1, len(s)):
-            if s[i] == s[i - 1]:
-                cnt += 1
-            else:
-                cnt = 1
-            ans = max(ans, cnt)
-        return ans
-
-    def duo(self, s: str, c1: str, c2: str) -> int:
-        pos: Dict[int, int] = {0: -1}
-        ans = 0
-        delta = 0
-        for i, ch in enumerate(s):
-            if ch != c1 and ch != c2:
-                pos.clear()
-                pos[0] = i
-                delta = 0
-                continue
-
-            if ch == c1:
-                delta += 1
-            else:
-                delta -= 1
-
-            if delta in pos:
-                ans = max(ans, i - pos[delta])
-            else:
-                pos[delta] = i
-
-        return ans
-
-    def trio(self, s: str) -> int:
-        cnt0 = cnt1 = cnt2 = 0  
-        pos: Dict[Tuple[int, int], int] = {(0, 0): -1}
-        ans = 0
-        for i, ch in enumerate(s):
-            if ch == 'a':
-                cnt0 += 1
-            elif ch == 'b':
-                cnt1 += 1
-            else:  
-                cnt2 += 1
-
-            key = (cnt1 - cnt0, cnt2 - cnt0)
-
-            if key in pos:
-                ans = max(ans, i - pos[key])
-            else:
-                pos[key] = i
-
-        return ans
-
     def longestBalanced(self, s: str) -> int:
+        def mono(s):
+            r = 1
+            ch = s[0]
+            cur = 0
+            for i in s:
+                if i == ch:
+                    cur += 1
+                else:
+                    r = max(r, cur)
+                    ch = i
+                    cur = 1
+            r = max(r, cur)
+            return r
+        
+
+        def duos(s, c1, c2):
+            r = 0
+            balance = 0
+            d = {0: -1}
+            for i in range(len(s)):
+                if s[i] == c1:
+                    balance += 1
+                else:
+                    balance -= 1
+
+                if balance in d:
+                    r = max(r, i - d[balance])
+                else:
+                    d[balance] = i
+            return r
+
+        def duo(s, c1, c2, c3):
+            li = s.split(c3)
+            r = 0
+            for i in li:
+                r = max(r, duos(i, c1, c2))
+            return r
+
+
+        def trio(s, c1, c2, c3):
+            d = {(0, 0) : -1}
+            r = 0
+            b1, b2 = 0, 0
+            for i in range(len(s)):
+                if s[i] == c1:
+                    b1 += 1
+                elif s[i] == c2:
+                    b2 += 1
+                    b1 -= 1
+                else:
+                    b2 -= 1
+                
+                if (b1, b2) in d:
+                    r = max(r, i - d[(b1, b2)])
+                else:
+                    d[(b1, b2)] = i
+            return r
+
+
         return max(
-            self.mono(s),
-            self.duo(s, 'a', 'b'),
-            self.duo(s, 'a', 'c'),
-            self.duo(s, 'b', 'c'),
-            self.trio(s),
+            mono(s),
+            duo(s, 'a', 'b', 'c'),
+            duo(s, 'a', 'c', 'b'),
+            duo(s, 'b', 'c', 'a'),
+            trio(s, 'a', 'b', 'c'),
         )
