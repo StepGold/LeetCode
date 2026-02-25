3d216de71c00345ca92d16999b2d12006918be75
Step <stepgold0@gmail.com>
2026-02-25 13:35:02 +0300

    Time: 7 ms (41.60%) | Memory: 19.4 MB (44.54%) - LeetSync

---
+++ 1458-sort-integers-by-the-number-of-1-bits/sort-integers-by-the-number-of-1-bits.py
@@ -0,0 +1,21 @@
+class Solution:
+    def sortByBits(self, arr: List[int]) -> List[int]:
+        m = max(arr)
+        r = 0
+        i = 0
+        while 2**i < m:
+            i += 1
+        li = [[] for _ in range(i+1)]
+        masks = [1 << j for j in range(i+1)]
+        
+        for i in arr:
+            r = 0
+            for j in masks:
+                if i & j:
+                    r += 1
+            li[r].append(i)
+
+        r = []
+        for i in li:
+            r+= list(sorted(i))
+        return r
